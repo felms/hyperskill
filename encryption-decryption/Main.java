652d6164295800a168c13cfddcd46ab56e656bbd
Felipe <felipemoreira1@gmail.com>
2020-05-17 19:11:08 -0300

    Project Encryption-Decryption: Stage 3/6: Decrypted!

--- encryption-decryption/Main.java
+++ encryption-decryption/Main.java
@@ -5,10 +5,20 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
 
+        String operation = scanner.nextLine();
+        String operationEnc = "enc";
+        String operationDec = "dec";
         String message = scanner.nextLine();
         int key = scanner.nextInt();
 
-        String encryptedMessage = encrypt(message, key);        
+
+        String encryptedMessage = "";
+        if (operation.equals(operationEnc)) {
+            encryptedMessage = encrypt(message, key); 
+        } else if (operation.equals(operationDec)) {
+            encryptedMessage = decrypt(message, key);
+        }
+               
         
         System.out.println(encryptedMessage.toString());
     }
@@ -16,23 +26,25 @@ public class Main {
 
     public static String encrypt(String message, int key) {
 
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int aSize = alphabet.length();
         StringBuilder encryptedMessage = new StringBuilder();
         
         for(char c : message.toCharArray()) {
-            int pos = alphabet.indexOf(c);
-            char d;
-            if(pos >= 0) {
-                int newPos = (pos + key) % aSize;
-                d = alphabet.charAt(newPos);
-            } else {
-                d = c;
-            }
-            
+            char d = (char) (c + key);
             encryptedMessage.append(d);
         }
 
         return encryptedMessage.toString();
+    }
+
+    public static String decrypt(String message, int key) {
+
+        StringBuilder decryptedMessage = new StringBuilder();
+        
+        for(char c : message.toCharArray()) {
+            char d = (char) (c - key);
+            decryptedMessage.append(d);
+        }
+
+        return decryptedMessage.toString();
     }
 }
